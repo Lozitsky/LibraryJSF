c3ef35d2cbc4920397cb157811da2f947b375a5a
Kirilo Lozitsky <bondxxl01@gmail.com>
2020-09-14 05:46:24 +0300

    add Resource Bundle and hack method

--- src/com/kirilo/beans/User.java
+++ src/com/kirilo/beans/User.java
@@ -1,10 +1,9 @@
 package com.kirilo.beans;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
-
-@Named("person")
+@ManagedBean
 @RequestScoped
 public class User {
     private String username;
